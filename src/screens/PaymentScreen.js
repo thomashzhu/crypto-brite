84b7b02493d98ddc25757a9418fa8b7b3ed5170b
Ryanliszewski <ryanliszewski@gmail.com>
2018-03-18 07:20:03 -0700

    Updated payment screen

--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -7,15 +7,72 @@ export default class componentName extends Component {
     super(props);
     this.state = {
       isPaymentLoading: true,
-      payment: null, 
+      payment: null,
+      isPaidStatus: null 
     }
   }
 
   componentDidMount(){
-    this.createPayment()  
+    this.postPayment()  
   }
 
-  async createPayment() {
+  async getPaymentStatus() {  
+
+    var details = {
+      paymentID: this.state.payment["payment"].txn_id
+    }
+
+    var formBody = []
+
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+
+    formBody = formBody.join("&");
+
+    try {
+      let response = await fetch(`https://brite-payment.herokuapp.com/api/payment/status`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: formBody
+      });
+
+      let responseJSON = null
+      
+      console.log(response.status)
+
+      if (response.status === 200) {
+        responseJSON = await response.json();
+        console.log(responseJSON)
+
+        this.setState({
+          isPaidStatus: Boolean(Number(responseJSON.paymentStatus))
+        })
+
+      } else {
+        responseJSON = await response.json();
+        const error = responseJSON.message
+
+        console.log(responseJSON)
+
+        this.setState({ errors: responseJSON.errors })
+        Alert.alert('Unable to get your feed', `Reason.. ${error}!`)
+      }
+    } catch (error) {
+      this.setState({ isLoading: false, response: error })
+
+      console.log(error)
+
+      Alert.alert('Unable to get the feed. Please try again later')
+    }
+  }
+
+  async postPayment() {
 
     var details = {
       'amount': 0.1,
@@ -54,7 +111,7 @@ export default class componentName extends Component {
           payment: responseJSON,
         })
 
-        console.log(this.state.payment["payment"].address);
+        this.getPaymentStatus()
 
       } else {
         responseJSON = await response.json();
@@ -88,8 +145,9 @@ export default class componentName extends Component {
             height: 300,
           }}
         />
+
         <Text> Address: {payment["payment"].address} </Text>
-        <Text> Amount </Text>
+        <Text> Amount: {payment["payment"].amount}</Text>
         </View>
       }
       </View>
